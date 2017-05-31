386098c0c54607d65bca336317efcb2b7374d62f
francescacoo <francescacoo@gmail.com>
2017-05-31 22:51:09 +0100

    commit

---
+++ array_sort.php
@@ -0,0 +1,17 @@
+<?php
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d",$n);
+$height_temp = fgets($handle);
+$height = explode(" ",$height_temp);
+array_walk($height,'intval');
+
+sort($height,SORT_NUMERIC);
+$tot=1;
+for($i=$n-1;$i--;$i=0){
+    if($height[$i]==$height[$n-1]){
+        $tot++;
+    }
+}
+echo $tot;
+?>
